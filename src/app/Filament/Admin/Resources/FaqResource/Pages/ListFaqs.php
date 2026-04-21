2d0e631a922e884424aa44fc801c5c47cc9e19db
Jefry Sunupurwa Asri <djambred@gmail.com>
2026-04-21 15:07:16 +0700

    added faq button

--- src/app/Filament/Admin/Resources/FaqResource/Pages/ListFaqs.php
+++ src/app/Filament/Admin/Resources/FaqResource/Pages/ListFaqs.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Admin\Resources\FaqResource\Pages;
 
 use App\Filament\Admin\Resources\FaqResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListFaqs extends ListRecords
 {
     protected static string $resource = FaqResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
